70e517e51261989d7116935b881c2dd9380c9c34
Jack Pena <penajackc@gmail.com>
2021-12-15 10:55:32 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const goodbyeMain = document.getElementById('main');
+goodbyeMain.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "Jack is the champion";
+document.body.append(newHeader);
+
+
+
